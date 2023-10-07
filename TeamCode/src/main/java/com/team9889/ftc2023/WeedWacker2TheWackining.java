c76f630fbadc5a45a88dcc3f18021798584f6751
PaperMarioTheThousandYearFan <147260631+PaperMarioTheThousandYearFan@users.noreply.github.com>
2023-10-07 12:23:51 -0400

    Did Some Stuff. Added First OP Mode

---
+++ TeamCode/src/main/java/com/team9889/ftc2023/WeedWacker2TheWackining.java
@@ -0,0 +1,13 @@
+package com.team9889.ftc2023;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class WeedWacker2TheWackining extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
