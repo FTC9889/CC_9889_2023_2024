c23289ad62efdf813e930c970a1f941d1646ecb2
PaperMarioTheThousandYearFan <147260631+PaperMarioTheThousandYearFan@users.noreply.github.com>
2023-11-11 13:20:52 -0500

    Added More Stuff Relating To The Color Sensor.

---
+++ TeamCode/src/main/java/com/team9889/ftc2023/opmode/TestingSenors.java
@@ -0,0 +1,121 @@
+package com.team9889.ftc2023.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+public class TestingSenors extends LinearOpMode{
+    NormalizedColorSensor colorSensor;
+    TouchSensor touchSensor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        touchSensor = hardwareMap.get(TouchSensor.class, "sensor_touch");
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+        waitForStart();
+        while(opModeIsActive()){
+            telemetry.addData("color red", colorSensor.getNormalizedColors().red);
+            telemetry.addData("color blue", colorSensor.getNormalizedColors().blue);
+            telemetry.addData("color green", colorSensor.getNormalizedColors().green);
+            if (touchSensor.isPressed()) {
+                telemetry.addData("Touch Sensor", "Is Pressed");
+            } else {
+                telemetry.addData("Touch Sensor", "Is Not Pressed");
+            };
+            telemetry.update();
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
