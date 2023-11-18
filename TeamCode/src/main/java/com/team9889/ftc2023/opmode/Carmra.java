08b25357c1bdc133bdb1ea372dc1a512e11bdcec
Daniel-9889 <147260536+Daniel-9889@users.noreply.github.com>
2023-11-18 13:21:47 -0500

    Added Sensor Tests

---
+++ TeamCode/src/main/java/com/team9889/ftc2023/opmode/Carmra.java
@@ -0,0 +1,4 @@
+package com.team9889.ftc2023.opmode;
+
+public class Carmra {
+}
--- TeamCode/src/main/java/com/team9889/ftc2023/opmode/Testing_something.java
+++ TeamCode/src/main/java/com/team9889/ftc2023/opmode/Testing_something.java
@@ -1,28 +1,47 @@
 package com.team9889.ftc2023.opmode;
 
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
-import com.qualcomm.robotcore.hardware.TouchSensor;
 
+@TeleOp
 public class Testing_something extends LinearOpMode{
-    NormalizedColorSensor colorSensor;
-    TouchSensor touchSensor;
+    NormalizedColorSensor colorsensor1, colorsensor2;
+    DcMotor test;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
-        touchSensor = hardwareMap.get(TouchSensor.class, "sensor_touch");
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+
+        colorsensor1 = hardwareMap.get(RevColorSensorV3.class, "color_sensor1");
+        colorsensor2 = hardwareMap.get(RevColorSensorV3.class, "color_sensor2");
+        test = hardwareMap.get(DcMotorEx.class, "test");
+
+        AnalogInput analogInput = hardwareMap.get(AnalogInput.class,"analoginput");
+
         waitForStart();
+        int target = 0;
         while(opModeIsActive()){
-            telemetry.addData("color red", colorSensor.getNormalizedColors().red);
-            telemetry.addData("color blue", colorSensor.getNormalizedColors().blue);
-            telemetry.addData("color green", colorSensor.getNormalizedColors().green);
-            if (touchSensor.isPressed()) {
-                telemetry.addData("Touch Sensor", "Is Pressed");
-            } else {
-                telemetry.addData("Touch Sensor", "Is Not Pressed");
-            };
+
+            test.setTargetPosition(target);
+            test.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            test.setPower(1);
+            test.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+            if (!test.isBusy())
+                target+=1000;
+
+            telemetry.addData("color red", colorsensor1.getNormalizedColors().red);
+            telemetry.addData("color blue", colorsensor1.getNormalizedColors().blue);
+            telemetry.addData("color green", colorsensor1.getNormalizedColors().green);
+            telemetry.addData("color red", colorsensor2.getNormalizedColors().red);
+            telemetry.addData("color blue", colorsensor2.getNormalizedColors().blue);
+            telemetry.addData("color green", colorsensor2.getNormalizedColors().green);
+            telemetry.addData("breakbeam",analogInput.getVoltage());
             telemetry.update();
 
 
