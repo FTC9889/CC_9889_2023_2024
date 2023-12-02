993627ce51305f6a75c2c504d6be4bd5a843988d
Daniel-9889 <147260536+Daniel-9889@users.noreply.github.com>
2023-12-02 11:48:18 -0500

    Added Logic To Intake

--- TeamCode/src/main/java/com/team9889/ftc2023/subsystems/Intake.java
+++ TeamCode/src/main/java/com/team9889/ftc2023/subsystems/Intake.java
@@ -1,19 +1,44 @@
 package com.team9889.ftc2023.subsystems;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Intake {
     DcMotor intake, extend;
-
+    DigitalChannel digitalTouch;
+    Servo vfb, gate;
     public void init(HardwareMap hardwareMap) {
         intake = hardwareMap.dcMotor.get("intake");
-        extend = hardwareMap.dcMotor.get("iextend");
+        extend = hardwareMap.dcMotor.get("extend");
 
+        vfb = hardwareMap.servo.get("lift");
+        gate = hardwareMap.servo.get("gate");
+
+        digitalTouch = hardwareMap.digitalChannel.get("liftmagnet");
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+        extend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
     }
     //turn on intake
     //turn off intake
-
+    public void setPower(double power){
+        if(power < 0) {
+            if (digitalTouch.getState() ==false) {
+                extend.setPower(power);
+            } else {
+                extend.setPower(0);
+                extend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                extend.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
+        } else {
+            if (extend.getCurrentPosition() < 3500){
+                extend.setPower(power);
+            } else {
+                extend.setPower(0);
+            }
+        }
+    }
     public void on() {
         intake.setPower(1);
     }
@@ -22,8 +47,41 @@ public class Intake {
         intake.setPower(0);
     }
 
+    public void slowOn(){
+        intake.setPower(0.2);
+    }
+
     public void out() {
         intake.setPower(-1);
     }
+
+    public void openGate(){
+        gate.setPosition(0);
+    }
+    public void closeGate(){
+        gate.setPosition(1);
+    }
+
+
+    public void vfbUp(){
+        vfb.setPosition(1);
+    }
+    public void vfbDown(){
+        vfb.setPosition(0);
+    }
+
+    public void startIntake(){
+        closeGate();
+        vfbDown();
+        on();
+    }
+
+    public void stopIntake(){
+       vfbUp();
+       slowOn();
+       
+    }
+
+
 }
 
