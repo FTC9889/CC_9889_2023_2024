8a3cf95f0035afb6d6a0768ce93159e0d46f1070
Daniel-9889 <147260536+Daniel-9889@users.noreply.github.com>
2023-12-30 13:51:04 -0500

    Update EasyOpenCVCamera.java

--- TeamCode/src/main/java/com/team9889/ftc2023/camera/EasyOpenCVCamera.java
+++ TeamCode/src/main/java/com/team9889/ftc2023/camera/EasyOpenCVCamera.java
@@ -8,42 +8,91 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 public class EasyOpenCVCamera extends OpenCvPipeline {
-    Telemetry telemetry;
-    Mat mat = new Mat();
-    static final Rect LEFT_ROI = new Rect(
-        new Point(0, 0),
-        new Point(100,100)
-    );
-    static final Rect RIGHT_ROI = new Rect(
-            new Point(0, 0),
-            new Point(100,100)
-    );
-    static double PERCENT_COLLOR_THRESHHOLD = 0.4;
+    /*
+     * These are our variables that will be
+     * modifiable from the variable tuner.
+     *
+     * Scalars in OpenCV are generally used to
+     * represent color. So our values in the
+     * lower and upper Scalars here represent
+     * the Y, Cr and Cb values respectively.
+     *
+     * YCbCr, like most color spaces, range
+     * from 0-255, so we default to those
+     * min and max values here for now, meaning
+     * that all pixels will be shown.
+     */
+    public Scalar lower = new Scalar(0, 0, 0);
+    public Scalar upper = new Scalar(255, 255, 255);
 
+    /*
+     * A good practice when typing EOCV pipelines is
+     * declaring the Mats you will use here at the top
+     * of your pipeline, to reuse the same buffers every
+     * time. This removes the need to call mat.release()
+     * with every Mat you create on the processFrame method,
+     * and therefore, reducing the possibility of getting a
+     * memory leak and causing the app to crash due to an
+     * "Out of Memory" error.
+     */
+    private Mat ycrcbMat       = new Mat();
+    private Mat binaryMat      = new Mat();
+    private Mat maskedInputMat = new Mat();
 
-    public EasyOpenCVCamera(Telemetry t ) {telemetry = t; }
-
-    @Override()
+    @Override
     public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
-        Scalar lowhsv = new Scalar(0, 0, 0);
-        Scalar highhsv = new Scalar(255, 255, 255);
+        /*
+         * Converts our input mat from RGB to YCrCb.
+         * EOCV ALWAYS returns RGB mats, so you'd
+         * always convert from RGB to the color
+         * space you want to use.
+         *
+         * Takes our "input" mat as an input, and outputs
+         * to a separate Mat buffer "ycrcbMat"
+         */
+        Imgproc.cvtColor(input, ycrcbMat, Imgproc.COLOR_RGB2YCrCb);
 
-        Core.inRange(mat, lowhsv, highhsv, mat);
-        Mat left = mat.submat(LEFT_ROI);
-        Mat right = mat.submat(RIGHT_ROI);
+        /*
+         * This is where our thresholding actually happens.
+         * Takes our "ycrcbMat" as input and outputs a "binary"
+         * Mat to "binaryMat" of the same size as our input.
+         * "Discards" all the pixels outside the bounds specified
+         * by the scalars above (and modifiable with EOCV-Sim's
+         * live variable tuner.)
+         *
+         * Binary meaning that we have either a 0 or 255 value
+         * for every pixel.
+         *
+         * 0 represents our pixels that were outside the bounds
+         * 255 represents our pixels that are inside the bounds
+         */
+        Core.inRange(ycrcbMat, lower, upper, binaryMat);
 
-        double leftValue = Core.sumElems(left).val[0] / LEFT_ROI.area() / 255;
-        double rightValue = Core.sumElems(left).val[0] / RIGHT_ROI.area() / 255;
+        /*
+         * Release the reusable Mat so that old data doesn't
+         * affect the next step in the current processing
+         */
+        maskedInputMat.release();
 
-        left.release();;
-        right.release();
+        /*
+         * Now, with our binary Mat, we perform a "bitwise and"
+         * to our input image, meaning that we will perform a mask
+         * which will include the pixels from our input Mat which
+         * are "255" in our binary Mat (meaning that they're inside
+         * the range) and will discard any other pixel outside the
+         * range (RGB 0, 0, 0. All discarded pixels will be black)
+         */
+        Core.bitwise_and(input, input, maskedInputMat, binaryMat);
 
-        telemetry.addData("Left raw value", (int) Core.sumElems(left).val[0]);
-        telemetry.addData("Right raw value", (int) Core.sumElems(right).val[0]);
-        telemetry.addData("Left percentage", Math.round(leftValue * 100) + "%");
-        telemetry.addData("Right percentage", Math.round(rightValue * 100) + "%");
-
-        return null;
+        /*
+         * The Mat returned from this method is the
+         * one displayed on the viewport.
+         *
+         * To visualize our threshold, we'll return
+         * the "masked input mat" which shows the
+         * pixel from the input Mat that were inside
+         * the threshold range.
+         */
+        return maskedInputMat;
     }
 }
