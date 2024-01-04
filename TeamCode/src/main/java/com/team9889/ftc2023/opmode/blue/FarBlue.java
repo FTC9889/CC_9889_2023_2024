3382e4088856cfb4921b1fb46bf73c2a2c6ad4ac
Daniel-9889 <147260536+Daniel-9889@users.noreply.github.com>
2024-01-03 22:16:55 -0500

    Upgraded Far Red And Far Blue Center

--- TeamCode/src/main/java/com/team9889/ftc2023/opmode/blue/FarBlue.java
+++ TeamCode/src/main/java/com/team9889/ftc2023/opmode/blue/FarBlue.java
@@ -2,6 +2,7 @@ package com.team9889.ftc2023.opmode.blue;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.team9889.ftc2023.opmode.red.FarRed;
 import com.team9889.ftc2023.subsystems.Robot;
 @Autonomous
 public class FarBlue extends LinearOpMode {
@@ -55,6 +56,42 @@ public class FarBlue extends LinearOpMode {
             mRobot.mDrive.brake();
             
         }
+        if (side == FarBlue.BackDrop.CENTER) {
+            mRobot.mDrive.setPower(0, 0.5, 0);
+            sleep(830);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.reset_encoder();
+            mRobot.mDrive.setPower(0.5, 0, 0);
+            sleep(2000);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0, 0.5, 0);
+            while (mRobot.mDrive.front_encoder() < 850 && opModeIsActive()) sleep(50);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.reset_encoder();
+            mRobot.mDrive.setPower(-0.5, 0, 0);
+            sleep(8900);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0, -0.5, 0);
+            while (mRobot.mDrive.front_encoder() < 830 && opModeIsActive()) sleep(50);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.reset_encoder();
+            mRobot.mDrive.setPower(0, 0, 0.5);
+            sleep(???);
+            mRobot.mDrive.brake();
+            mRobot.mLift.score_position();
+            mRobot.mDrive.setPower(0, 0.5, 0);
+            sleep(800);
+            mRobot.mDrive.brake();
+            mRobot.mLift.set_Grabber_Open(true, true);
+            sleep(500);
+            mRobot.mLift.intake_position();
+            mRobot.mDrive.setPower(-0.5, 0, 0);
+            sleep(2500);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0, 0.5, 0);
+            sleep(700);
+            mRobot.mDrive.brake();
+        }
 
         if (side == FarBlue.BackDrop.RIGHT) {
             mRobot.mDrive.setPower(0, 0.5, 0);
@@ -75,7 +112,7 @@ public class FarBlue extends LinearOpMode {
             mRobot.mDrive.reset_encoder();
             mRobot.mDrive.setPower(-0.5, 0, 0);
             sleep(side_tile + 500);
-            mRobot.mDrive.brake();
+                        mRobot.mDrive.brake();
             mRobot.mLift.score_position();
             mRobot.mLift.set_Grabber_Open(true, true);
             mRobot.mLift.intake_position();
@@ -87,42 +124,6 @@ public class FarBlue extends LinearOpMode {
             mRobot.mDrive.brake();
         }
 
-        if (side == FarBlue.BackDrop.CENTER) {
-            mRobot.mDrive.setPower(0, 0.5, 0);
-            sleep(830);
-            mRobot.mDrive.brake();
-            mRobot.mDrive.reset_encoder();
-            mRobot.mDrive.setPower(0.5, 0, 0);
-            sleep(2000);
-            mRobot.mDrive.brake();
-            mRobot.mDrive.setPower(0, 0.5, 0);
-            while (mRobot.mDrive.front_encoder() < 850 && opModeIsActive()) sleep(50);
-            mRobot.mDrive.brake();
-            mRobot.mDrive.reset_encoder();
-            mRobot.mDrive.setPower(-0.5, 0, 0);
-            sleep(8900);
-            mRobot.mDrive.brake();
-            mRobot.mDrive.setPower(0, -0.5, 0);
-            while (mRobot.mDrive.front_encoder() < 830 && opModeIsActive()) sleep(50);
-            mRobot.mDrive.brake();
-            mRobot.mDrive.reset_encoder();
-            mRobot.mDrive.setPower(0, 0, -0.5);
-            sleep(???);
-            mRobot.mDrive.brake();
-            mRobot.mLift.score_position();
-            mRobot.mDrive.setPower(0, 0.5, 0);
-            sleep(800);
-            mRobot.mDrive.brake();
-            mRobot.mLift.set_Grabber_Open(true, true);
-            sleep(500);
-            mRobot.mLift.intake_position();
-            mRobot.mDrive.setPower(-0.5, 0, 0);
-            sleep(2500);
-            mRobot.mDrive.brake();
-            mRobot.mDrive.setPower(0, 0.5, 0);
-            sleep(700);
-            mRobot.mDrive.brake();
-        }
 
 //        mRobot.mDrive.setPower(-0.5, 0, 0);
 //        sleep(5000);
