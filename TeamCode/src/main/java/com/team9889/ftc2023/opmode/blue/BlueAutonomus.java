7a8281b0c9dbc6bb11f61542515a8c5b322ee0b0
Daniel-9889 <147260536+Daniel-9889@users.noreply.github.com>
2024-01-04 17:42:59 -0500

    Added Left For Red And Blue Autonomus

--- TeamCode/src/main/java/com/team9889/ftc2023/opmode/blue/BlueAutonomus.java
+++ TeamCode/src/main/java/com/team9889/ftc2023/opmode/blue/BlueAutonomus.java
@@ -27,6 +27,39 @@ public class BlueAutonomus extends LinearOpMode {
         long side_tile = 2300;
 
         if(side == BlueAutonomus.BackDrop.LEFT) {
+            mRobot.mDrive.setPower(0, 0.5, 0);
+            sleep(tile);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0, 0, -0.5);
+            sleep(???);
+            mRobot.mDrive.brake();
+            mRobot.mIntake.out();
+            mRobot.mIntake.off();
+            mRobot.mDrive.setPower(-0.5, 0, 0);
+            sleep(side_tile - 300);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0, 0, 0.5);
+            sleep(???);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0, -0.5, 0);
+            sleep(tile +400);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0.5, 0, 0);
+            sleep(2000);
+            mRobot.mDrive.brake();
+            mRobot.mLift.score_position();
+            mRobot.mDrive.setPower(0, -0.5, 0);
+            sleep(440);
+            mRobot.mDrive.brake();
+            mRobot.mLift.set_Grabber_Open(true, true);
+            sleep(500);
+            mRobot.mLift.intake_position();
+            mRobot.mDrive.setPower(0.5, 0, 0);
+            sleep(3000);
+            mRobot.mDrive.brake();
+            mRobot.mDrive.setPower(0, -0.5, 0);
+            sleep(500);
+            mRobot.mDrive.brake();
 
         }
 
